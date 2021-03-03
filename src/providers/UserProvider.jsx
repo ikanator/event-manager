12347ffe330078a5a082a987b8faa57fa9167c96
Irakliy Katsadze <ikatsadze@Irakliys-MacBook-Pro.local>
2021-03-03 19:12:59 +0200

    catch errors

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,31 +1,37 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export const UserContext = createContext({ user: null });
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const { push } = useHistory();
 
   useEffect(() => {
     const checkSession = async () => {
-      const { data: { user, success } = {} } = await axios({
-        method: "GET",
-        url: `https://stark-garden-96861.herokuapp.com/auth/facebook/success`,
-        withCredentials: true,
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Credentials": true,
-        },
-      });
+      try {
+        const { data: { user, success } = {} } = await axios({
+          method: "GET",
+          url: `https://stark-garden-96861.herokuapp.com/auth/facebook/success`,
+          withCredentials: true,
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+          },
+        });
 
-      if (user && success) {
-        setUser(user);
+        if (user && success) {
+          setUser(user);
+        }
+      } catch (err) {
+        push("/login");
       }
     };
 
     checkSession();
-  }, []);
+  }, [push]);
 
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
