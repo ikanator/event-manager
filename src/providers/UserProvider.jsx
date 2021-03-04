f62ccc637220285961dfb7d698b548906f321e31
Irakliy Katsadze <ikatsadze@Irakliys-MacBook-Pro.local>
2021-03-04 11:58:20 +0200

    update user provider

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,24 +1,20 @@
 import { useState, useEffect, createContext } from "react";
-import { useHistory } from "react-router-dom";
 
 export const UserContext = createContext({ user: null });
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const { push } = useHistory();
 
   useEffect(() => {
-    const checkSession = async () => {
-      const userId = localStorage.getItem("userId");
-      const tokenId = localStorage.getItem("tokenId");
+    const userId = localStorage.getItem("userId");
+    const tokenId = localStorage.getItem("tokenId");
 
-      if (userId && tokenId) {
-        setUser({ userId });
-      }
-    };
-
-    checkSession();
-  }, [push]);
+    if (userId && tokenId) {
+      setUser({ userId });
+    } else {
+      setUser(undefined);
+    }
+  }, []);
 
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
