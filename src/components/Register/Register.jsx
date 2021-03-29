889ffcb8d0d0fe410d8104f80ac8d095786b1d58
Irakliy Katsadze <ikatsadze@Irakliys-MacBook-Pro.local>
2021-03-29 19:41:56 +0300

    add registration form validation

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Container,
   Row,
@@ -8,34 +8,34 @@ import {
   Button,
 } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
+
+const REQUIRED_MESSAGE = "Це поле необхідне";
+const INITIAL_VALUES = {
+  email: "",
+  firstName: "",
+  lastName: "",
+  password: "",
+};
 
 export function Register() {
   const { push } = useHistory();
-  const [username, setUsername] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit, errors, reset } = useForm({
+    mode: "onBlur",
+    defaultValues: INITIAL_VALUES,
+  });
 
-  const register = async (event) => {
-    event.preventDefault();
+  const onSubmit = async (values) => {
     const { data: { user, success } = {} } = await axios({
       method: "POST",
       url: `${process.env.REACT_APP_API_URL}/auth/create`,
-      data: {
-        username,
-        firstName,
-        lastName,
-        password,
-      },
+      data: values,
     });
     if (user && success) {
       push("/login");
     } else {
-      setUsername("");
-      setPassword("");
-      setFirstName("");
-      setLastName("");
+      reset(INITIAL_VALUES);
     }
   };
 
@@ -51,48 +51,91 @@ export function Register() {
                 </h1>
               </Row>
               <div className="pt-3 pb-3">
-                <Form className="form-horizontal">
+                <Form
+                  className="form-horizontal"
+                  onSubmit={handleSubmit(onSubmit)}
+                >
                   <FormGroup className="row justify-content-center px-3">
                     <div className="col-9 px-0">
                       <FormControl
+                        ref={register({
+                          required: {
+                            value: true,
+                            message: REQUIRED_MESSAGE,
+                          },
+                          validate: (value) => {
+                            const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            return re.test(String(value).toLowerCase());
+                          },
+                        })}
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        className={
+                          errors?.email || errors?.email?.message
+                            ? "is-invalid"
+                            : "border-info"
+                        }
+                      />
+                      <p className="text-danger mb-0">
+                        {errors?.email?.message || errors?.email?.validate
+                          ? "Введіть правильний email"
+                          : ""}
+                      </p>
+                    </div>
+                  </FormGroup>
+                  <FormGroup className="row justify-content-center px-3">
+                    <div className="col-9 px-0">
+                      <FormControl
+                        ref={register({
+                          required: {
+                            value: true,
+                            message: REQUIRED_MESSAGE,
+                          },
+                        })}
+                        name="firstName"
                         type="text"
                         placeholder="Ім'я"
                         className="border-info"
-                        value={firstName}
-                        onChange={(event) => setFirstName(event.target.value)}
                       />
+                      <p className="text-danger mb-0">
+                        {errors?.firstName?.message}
+                      </p>
                     </div>
                   </FormGroup>
                   <FormGroup className="row justify-content-center px-3">
                     <div className="col-9 px-0">
                       <FormControl
+                        ref={register({
+                          required: {
+                            value: true,
+                            message: REQUIRED_MESSAGE,
+                          },
+                        })}
+                        name="lastName"
                         type="text"
                         placeholder="Прізвище"
                         className="border-info"
-                        value={lastName}
-                        onChange={(event) => setLastName(event.target.value)}
                       />
+                      <p className="text-danger mb-0">
+                        {errors?.lastName?.message}
+                      </p>
                     </div>
                   </FormGroup>
                   <FormGroup className="row justify-content-center px-3">
                     <div className="col-9 px-0">
                       <FormControl
-                        type="text"
-                        placeholder="Ім'я користувача"
-                        className="border-info"
-                        value={username}
-                        onChange={(event) => setUsername(event.target.value)}
-                      />
-                    </div>
-                  </FormGroup>
-                  <FormGroup className="row justify-content-center px-3">
-                    <div className="col-9 px-0">
-                      <FormControl
-                        value={password}
-                        onChange={(event) => setPassword(event.target.value)}
+                        ref={register({
+                          required: { value: true, message: REQUIRED_MESSAGE },
+                        })}
+                        name="password"
                         type="password"
+                        placeholder="Пароль"
                         className="border-info"
                       />
+                      <p className="text-danger mb-0">
+                        {errors?.password?.message}
+                      </p>
                     </div>
                   </FormGroup>
                   <FormGroup className="row justify-content-center px-3">
@@ -100,7 +143,7 @@ export function Register() {
                       <Button
                         type="submit"
                         className="btn-block btn-info"
-                        onClick={register}
+                        onClick={handleSubmit(onSubmit)}
                       >
                         Створити акаунт
                       </Button>
