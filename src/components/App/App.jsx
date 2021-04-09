1b2978d0133ada0af97d0a7328c8b465239027e7
Irakli Katsadze <irakliy.katsadze@gmail.com>
2021-04-09 19:03:49 +0300

    implement header (#2)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
@@ -6,27 +7,35 @@ import { Events } from "../Events/Events";
 import { AuthenticatedRoute } from "../AuthenticatedRoute/AuthenticatedRoute";
 import { UserProvider } from "../../providers/UserProvider";
 import { Authenticated } from "../Authenticated/Authenticated";
+import Logo from "../../logo.png";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/authenticated">
-          <Authenticated />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <UserProvider>
-          <AuthenticatedRoute path="/" exact>
-            <Events />
-          </AuthenticatedRoute>
-        </UserProvider>
-      </Switch>
-    </Router>
+    <div className="h-100 d-flex flex-column">
+      <Router>
+        <Navbar bg="dark" variant="dark" sticky="top">
+          <Navbar.Brand>
+            <img width="55" src={Logo} alt="Event Manager Logo" /> Event Manager
+          </Navbar.Brand>
+        </Navbar>
+        <Switch>
+          <Route path="/authenticated">
+            <Authenticated />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <UserProvider>
+            <AuthenticatedRoute path="/" exact>
+              <Events />
+            </AuthenticatedRoute>
+          </UserProvider>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
